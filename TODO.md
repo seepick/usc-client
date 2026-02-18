a9b615abd4ff88e2b502f1dd9c7102f52c792bcc
Seepick <seepick@users.noreply.github.com>
2026-02-18 13:20:21 +0100

    fix test

--- TODO.md
+++ TODO.md
@@ -5,6 +5,7 @@
 * system tests (enable-able); only run locally (add bin/validate.sh file)
 * get fixtures dependency working properly
 * switch ktor client to Apache5; test throughly!
+* move test resources to src/test/kotlin next to the test class
 
 ## Nope
 
--- src/test/kotlin/com/github/seepick/uscclient/schedule/ScheduleParserTest.kt
+++ src/test/kotlin/com/github/seepick/uscclient/schedule/ScheduleParserTest.kt
@@ -4,25 +4,21 @@ import com.github.seepick.uscclient.readTestResponse
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.equals.shouldBeEqual
 
-class ScheduleParserTest : StringSpec() {
-    private fun parseSchedule(fileName: String) = ScheduleParser.parse(readTestResponse(fileName))
+class ScheduleParserTest : StringSpec({
 
-    init {
-        "When parse booked activities Then return entries" {
-            val schedule = parseSchedule("schedule.activities.html")
+    fun parseSchedule(fileName: String) =
+        ScheduleParser.parse(readTestResponse(fileName))
 
-            schedule shouldBeEqual listOf(
-                BookedOrScheduled(84742854, "studio-108-3", ScheduleEntityType.Activity),
-                BookedOrScheduled(84726253, "yoga-spot-olympisch-stadion", ScheduleEntityType.Activity),
-                BookedOrScheduled(84810748, "movements-city", ScheduleEntityType.Activity),
-            )
-        }
-        "When parse scheduled freetraining Then return entry" {
-            val schedule = parseSchedule("schedule.freetraining.html")
-
-            schedule shouldBeEqual listOf(
-                BookedOrScheduled(83664089, "vitality-spa-fitness-amsterdam", ScheduleEntityType.Freetraining),
-            )
-        }
+    "When parse booked activities Then return entries" {
+        parseSchedule("schedule.activities.html") shouldBeEqual listOf(
+            BookedActivity(84742854, "studio-108-3"),
+            BookedActivity(84726253, "yoga-spot-olympisch-stadion"),
+            BookedActivity(84810748, "movements-city"),
+        )
     }
-}
+    "When parse scheduled freetraining Then return entry" {
+        parseSchedule("schedule.freetraining.html") shouldBeEqual listOf(
+            ScheduledFreetraining(83664089, "vitality-spa-fitness-amsterdam"),
+        )
+    }
+})
